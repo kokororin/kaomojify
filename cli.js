516e8cb5289bf95a6a080d43217e6d68fa1c54e6
Kokororin <i@kotori.love>
2016-09-20 14:53:19 +0800

    🍭fix relative path issue

--- cli.js
+++ cli.js
@@ -8,37 +8,64 @@ const path = require('path');
 const sh = require('shelljs');
 const convert = require('./convert');
 
-const argv = yargs.argv;
+const ARGS = yargs
+  .describe('output', 'Output file')
+  .describe('version', 'Print version number and exit')
+  .help('help')
+  .alias('o', 'output')
+  .alias('h', 'help')
+  .alias('v', 'version')
+  .argv;
 
-if (!argv._[0]) {
-  console.error('ERROR: Invalid arguments');
-  process.exit(1);
-}
+const cwd = sh.pwd().toString();
 
-const input = argv._[0];
-
-const convertAndWrite = (file) => {
+const convertAndWrite = (file, outputPath) => {
 
   let output = fs.readFileSync(file, 'utf8');
   output = convert(output);
-  if (!argv.o) {
-    console.error('ERROR: Invalid -o');
-    process.exit(1);
+  const outputDir = path.join(cwd, '/' + outputPath);
+  if (!fs.existsSync(outputDir)) {
+    fs.mkdirSync(outputDir);
   }
-  const outputPath = path.join(sh.pwd(), '/' + argv.o);
-  if (!fs.existsSync(outputPath)) {
-    console.log(outputPath);
-    fs.mkdirSync(outputPath);
-  }
-  fs.writeFileSync(outputPath + path.basename(file), output);
+  fs.writeFileSync(outputDir + path.basename(file), output);
 };
 
-glob(input, (err, files) => {
-  if (err) {
-    console.error('ERROR: Invalid glob definition');
+if (ARGS.version || ARGS.v) {
+  const json = require('./package.json');
+  console.log(json.name + ' ' + json.version);
+  process.exit(0);
+}
+
+
+if (ARGS._[0]) {
+  const input = ARGS._[0];
+  let output = undefined;
+  if (ARGS.output || ARGS.o) {
+    output = ARGS.output || ARGS.o;
+  } else {
+    console.error('ERROR: Invalid output');
     process.exit(1);
   }
-  for (let file of files) {
-    convertAndWrite(file);
+  if (fs.existsSync(input)) {
+    convertAndWrite(input, output);
+    process.exit(0);
   }
-});
+  glob(input, (err, files) => {
+    if (err) {
+      console.error('ERROR: Invalid glob definition');
+      process.exit(1);
+    }
+    for (let file of files) {
+      convertAndWrite(file, output);
+    }
+  });
+  process.exit(0);
+}
+
+if ((ARGS.output || ARGS.o) && !ARGS._[0]) {
+  console.error('ERROR: Invalid input');
+  process.exit(1);
+}
+
+console.log('Usage: kaomojify <input> -o <output>');
+process.exit(0);
